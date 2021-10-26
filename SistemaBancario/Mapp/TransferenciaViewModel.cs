305f0fb9ede7508b355e1f712a152a7af3a3a112
Anna Carollyne <anna11bra@gmail.com>
2021-10-26 09:15:18 -0400

    Criada o mapping de transferencias

---
+++ SistemaBancario/Mapp/TransferenciaViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using SistemaBancario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaBancario.Mapp
+{
+    public class TransferenciaViewModel
+    {
+        public int Id { get; set; }
+        public double ValorDeTransferencia { get; set; }
+        public string NomeDoBanco { get; set; }
+        public int Agencia { get; set; }
+        public string Conta { get; set; }
+        public string NomeDoFavorecido { get; set; }
+        public string CpfCnpj { get; set; }
+        public DateTime DiaDaTransferencia { get; set; }
+        public Usuarios Usuarios { get; set; }
+        public Conta Contas { get; set; }
+
+        static void Main(string[] args)
+        {
+            //Configure and create an instace for the mapper
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Transferencias, TransferenciaViewModel>());
+            var mapper = config.CreateMapper();
+        }
+    }
+}
---
+++ SistemaBancario/Models/Transferencias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaBancario.Models
+{
+    public class Transferencias
+    {
+        public int Id { get; set; }
+        public double ValorDeTransferencia { get; set; }
+        public string NomeDoBanco { get; set; }
+        public int Agencia { get; set; }
+        public string Conta { get; set; }
+        public string NomeDoFavorecido { get; set; }
+        public string CpfCnpj { get; set; }
+        public DateTime DiaDaTransferencia { get; set; }
+        public Usuarios Usuarios { get; set; }
+        public Conta Contas { get; set; }
+    }
+}
